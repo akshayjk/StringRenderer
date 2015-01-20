06fc6cff09c0d1899c53538d65326d1b6573e36e
Akshay Kulkarni <akshayjk10@gmail.com>
2015-01-20 08:51:21 +0530

    Back End process structure
    Here is the simple backend server and some processes related to those

---
+++ StartServer.js
@@ -0,0 +1,37 @@
+/**
+ * Created by 385382 on 1/19/2015.
+ */
+/**
+ * Created by 290494 on 8/20/2014.
+ */
+
+// Node Modules
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    xml2js = require('xml2js');
+
+
+app.post("/postXml", function (req, res) {
+    var body = "";
+    req.on('data', function (data) {
+        body += data;
+    });
+    req.on('end', function () {
+        console.log("info", body);
+        var postData = body;
+        res.send(postData);
+        xml2js.parseString(postData, function(err, result){
+            console.log("result " + JSON.stringify(result));
+        })
+    });
+});
+
+app.get("/uploadLink", function (req, res) {
+    winston.log("info", "request received ")
+    //var responseHTML = '<html><head><title>upload</title></head><body><formaction="http://www.google.com"enctype="multipart/form-data"method="GET"><inputtype="file"name="upload"><br><inputtype="submit"value="upload"></form></body></html>';
+    //res.setHeader("Content-Type", "text/html");
+    res.sendfile('./Upload.html');
+})
+
+app.listen(5858);
